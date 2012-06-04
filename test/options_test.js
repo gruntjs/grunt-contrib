b088d15bcaac5b1bb67e34cc57bdddc0031c2274
Tyler Kellen <tyler@sleekcode.net>
2012-06-04 09:00:01 -0500

    test failover defaults

--- test/options_test.js
+++ test/options_test.js
@@ -26,11 +26,13 @@ grunt.loadTasks("../tasks");
 exports.options = {
   main: function(test) {
     var options = grunt.helper("options", {nameArgs: 'task:subtask'});
+    var options_with_default = grunt.helper("options", {nameArgs: 'task:subtask'}, {required:"default"});
 
-    test.expect(3);
-    test.equal('set', options.global, "should get params from global options.task key");
-    test.equal('subtask', options.setting, "should let params from global options.task.subtask override options.task");
-    test.equal('override all', options.param, "should allow task options key to override all others");
+    test.expect(4);
+    test.equal("set", options.global, "should get params from global options.task key");
+    test.equal("subtask", options.setting, "should let params from global options.task.subtask override options.task");
+    test.equal("override all", options.param, "should allow task options key to override all others");
+    test.equal("default", options_with_default.required, "should allow helper to define default values");
     test.done();
   }
 };
