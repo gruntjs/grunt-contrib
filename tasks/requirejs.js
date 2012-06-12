e1b932ba03f25be28643d45a8bde4e1221345c39
ctalkington <chris@christalkington.com>
2012-06-12 15:09:16 -0500

    requirejs: improve logic, logging, and error catching.

--- tasks/requirejs.js
+++ tasks/requirejs.js
@@ -3,23 +3,34 @@
  * Task: RequireJS
  * Description: Optimize RequireJS projects using r.js
  * Dependencies: requireJS
- * Contributor(s): @tbranyen, @tkellen
+ * Contributor(s): @tbranyen, @tkellen, @ctalkington
  *
  */
 
 module.exports = function(grunt) {
-  var requirejs = require("requirejs");
+  grunt.registerMultiTask("requirejs", "Build a RequireJS project.", function() {
+    var options = grunt.helper("options", this),
+        done = this.async();
 
-  grunt.registerMultiTask("requirejs", "Build a RequireJS project.", function(prop) {
-    var options = grunt.helper("options", this);
-    grunt.helper("r.js", options, function(resp) {
-      grunt.log.writeln(resp);
+    grunt.helper("requirejs", options, function(error, response) {
+      if (error === null) {
+        grunt.log.ok(response);
+      } else {
+        grunt.log.error(error);
+        grunt.fail.warn("RequireJS optimizer failed.");
+      }
+
+      done();
     });
-
   });
 
-  grunt.registerHelper("r.js", function(options, done) {
-    requirejs.optimize(options, done);
+  grunt.registerHelper("requirejs", function(options, callback) {
+    try {
+      require('requirejs').optimize(options, function(response) {
+        callback(null, response);
+      });
+    } catch (e) {
+      callback(e, null);
+    }
   });
-
 };
