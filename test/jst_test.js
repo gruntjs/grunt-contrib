623d8a3f9c22432f89fa8325d9858a920e49a4f9
ctalkington <chris@christalkington.com>
2012-06-26 00:50:30 -0500

    jst: update underscore logic and add new test to test output.

--- test/jst_test.js
+++ test/jst_test.js
@@ -2,12 +2,17 @@ var grunt = require("grunt");
 
 exports.jade = {
   main: function(test) {
-    test.expect(1);
+    test.expect(2);
 
     var expectA = "function";
     var sourceA = require("./fixtures/output/jst.js");
     var resultA = typeof sourceA["JST"]["fixtures/jst/template.html"];
     test.equal(expectA, resultA, "should compile underscore templates into JST");
+
+    var expectB = "<head><title>test</title></head>";
+    var sourceB = require("./fixtures/output/jst.js");
+    var resultB = sourceA["JST"]["fixtures/jst/template.html"]({title: "test"});
+    test.equal(expectB, resultB, "should output html when run");
 
     test.done();
   }
