42cdacd53a62adc70f2e231f9ade16c94a92fb8e
Tyler Kellen <tyler@sleekcode.net>
2012-06-28 12:16:54 -0500

    override requireJS logging to send output to grunt.log

--- tasks/requirejs.js
+++ tasks/requirejs.js
@@ -6,6 +6,17 @@
  */
 
 module.exports = function(grunt) {
+
+  var requirejs = require('requirejs');
+
+  // TODO: extend this to send build log to grunt.log.ok / grunt.log.error
+  // by overriding the r.js logger (or submit issue to r.js to expand logging support)
+  requirejs.define('node/print', [], function () {
+    return function print(msg) {
+      grunt.log(msg);
+    };
+  });
+
   grunt.registerMultiTask("requirejs", "Build a RequireJS project.", function() {
     var options = grunt.helper("options", this);
     require('requirejs').optimize(options);
