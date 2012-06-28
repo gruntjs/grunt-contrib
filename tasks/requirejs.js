659843a5016af70f851707d088f563c662202764
Tyler Kellen <tyler@sleekcode.net>
2012-06-28 10:07:07 -0500

    simplify

--- tasks/requirejs.js
+++ tasks/requirejs.js
@@ -1,36 +1,13 @@
-/*
- *
+/**
  * Task: RequireJS
  * Description: Optimize RequireJS projects using r.js
  * Dependencies: requireJS
- * Contributor(s): @tbranyen, @tkellen, @ctalkington
- *
+ * Contributor: @tbranyen
  */
 
 module.exports = function(grunt) {
   grunt.registerMultiTask("requirejs", "Build a RequireJS project.", function() {
     var options = grunt.helper("options", this);
-    var done = this.async();
-
-    grunt.helper("requirejs", options, function(error, response) {
-      if (error === null) {
-        grunt.log.ok(response);
-      } else {
-        grunt.log.error(error);
-        grunt.fail.warn("RequireJS optimizer failed.");
-      }
-
-      done();
-    });
-  });
-
-  grunt.registerHelper("requirejs", function(options, callback) {
-    try {
-      require('requirejs').optimize(options, function(response) {
-        callback(null, response);
-      });
-    } catch (e) {
-      callback(e, null);
-    }
+    require('requirejs').optimize(options);
   });
 };
