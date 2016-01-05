5933f84a47eecd577902403493dd542a99f530ae
Kyle Robinson Young <kyle@dontkry.com>
2016-01-04 17:11:01 -0800

    Update copyright to 2016

--- tasks/contrib.js
+++ tasks/contrib.js
@@ -2,7 +2,7 @@
  * grunt-contrib
  * http://gruntjs.com/
  *
- * Copyright (c) 2012 Tyler Kellen, contributors
+ * Copyright (c) 2016 Tyler Kellen, contributors
  * Licensed under the MIT license.
  * https://github.com/gruntjs/grunt-contrib/blob/master/LICENSE-MIT
  */
